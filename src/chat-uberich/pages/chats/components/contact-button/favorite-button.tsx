4e61fa51d602feed577c5f0c3d7e37d0fba83f21
Gabriela Quintaes <gabrielapquintaes@gmail.com>
2026-06-11 12:35:32 -0300

    feat: add evolution management features including modals for creating, deleting, and disconnecting accounts
    - Implemented the Evolution page with a table to display connected accounts.
    - Added modals for creating new accounts, displaying QR codes, and confirming deletions/disconnections.
    - Created services for managing client and evolution instances.
    - Introduced utility functions for chat performance optimization and message formatting.
    - Implemented caching mechanisms for performance improvements in chat transformations and message handling.

---
+++ src/chat-uberich/pages/chats/components/contact-button/favorite-button.tsx
@@ -0,0 +1,28 @@
+
+import { Tooltip } from "@/chat-uberich/components/ui/tooltip";
+import { Star } from "lucide-react";
+import { memo } from "react";
+
+interface IFavorite {
+  isFavorited: boolean;
+  onFavoriteChange: () => void;
+}
+
+export const FavoriteButton = memo(
+  ({ isFavorited, onFavoriteChange }: IFavorite) => {
+    return (
+      <Tooltip info="Favoritar" className="text-slate-200 bg-[#646464]">
+        <div
+          className="absolute bottom-[-5px] left-[-5px]"
+          onClick={onFavoriteChange}
+        >
+          {isFavorited ? (
+            <Star size={16} className="fill-red-500 text-red-500" />
+          ) : (
+            <Star size={16} className=" text-red-500" />
+          )}
+        </div>
+      </Tooltip>
+    );
+  },
+);
